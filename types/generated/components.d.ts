db9d6c9173d8487c9e87a50c7b384ce0947bc167
jddiaz <jddiaz@acreinsurance.com>
2024-06-07 17:07:17 -0500

    se sube dev-blog de strapi

---
+++ types/generated/components.d.ts
@@ -0,0 +1,129 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface BlogPostSelection extends Schema.Component {
+  collectionName: 'components_blog_post_selections';
+  info: {
+    displayName: 'postSelection';
+    icon: 'bulletList';
+  };
+  attributes: {
+    heading: Attribute.String;
+    featuredPosts: Attribute.Relation<
+      'blog.post-selection',
+      'oneToMany',
+      'api::post.post'
+    >;
+  };
+}
+
+export interface LayoutFeaturedCourse extends Schema.Component {
+  collectionName: 'components_layout_featured_courses';
+  info: {
+    displayName: 'featuredCourse';
+    icon: 'dashboard';
+    description: '';
+  };
+  attributes: {
+    course: Attribute.Relation<
+      'layout.featured-course',
+      'oneToOne',
+      'api::course.course'
+    >;
+    heading: Attribute.String;
+    announcement: Attribute.Text;
+  };
+}
+
+export interface LayoutHero extends Schema.Component {
+  collectionName: 'components_layout_heroes';
+  info: {
+    displayName: 'hero';
+    icon: 'layout';
+  };
+  attributes: {
+    callToAction: Attribute.String & Attribute.Required;
+    image: Attribute.Media<'images' | 'videos', true>;
+    buttons: Attribute.Component<'layout.link', true>;
+  };
+}
+
+export interface LayoutLink extends Schema.Component {
+  collectionName: 'components_layout_links';
+  info: {
+    displayName: 'link';
+    icon: 'arrowRight';
+  };
+  attributes: {
+    label: Attribute.String & Attribute.Required;
+    url: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface LayoutMission extends Schema.Component {
+  collectionName: 'components_layout_missions';
+  info: {
+    displayName: 'mission';
+    icon: 'layer';
+  };
+  attributes: {
+    heading: Attribute.String & Attribute.Required;
+    content: Attribute.Text & Attribute.Required;
+    showLogo: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<true>;
+  };
+}
+
+export interface LayoutNewsletterForm extends Schema.Component {
+  collectionName: 'components_layout_newsletter_forms';
+  info: {
+    displayName: 'newsletterForm';
+    icon: 'envelop';
+  };
+  attributes: {
+    heading: Attribute.String & Attribute.Required;
+    subHeading: Attribute.Text;
+  };
+}
+
+export interface LayoutServicesPreview extends Schema.Component {
+  collectionName: 'components_layout_services_previews';
+  info: {
+    displayName: 'servicesPreview';
+    icon: 'shoppingCart';
+  };
+  attributes: {
+    services: Attribute.Relation<
+      'layout.services-preview',
+      'oneToMany',
+      'api::service.service'
+    >;
+  };
+}
+
+export interface SeoSeoInformation extends Schema.Component {
+  collectionName: 'components_seo_seo_informations';
+  info: {
+    displayName: 'seoInformation';
+    icon: 'search';
+  };
+  attributes: {
+    seoTitle: Attribute.String;
+    seoDescription: Attribute.Text;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'blog.post-selection': BlogPostSelection;
+      'layout.featured-course': LayoutFeaturedCourse;
+      'layout.hero': LayoutHero;
+      'layout.link': LayoutLink;
+      'layout.mission': LayoutMission;
+      'layout.newsletter-form': LayoutNewsletterForm;
+      'layout.services-preview': LayoutServicesPreview;
+      'seo.seo-information': SeoSeoInformation;
+    }
+  }
+}
